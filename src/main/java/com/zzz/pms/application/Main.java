f2c4a0e5a8a860e1ce16ee0e389b3305411576d2
wwh <hello@hello.com>
2024-01-07 18:57:51 +0800

    add

---
+++ src/main/java/com/zzz/pms/application/Main.java
@@ -0,0 +1,18 @@
+package com.zzz.pms.application;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+}
